9171e58cfefbb1734b604a16158e7987d220b3be
why <triwahyuprasetyo@gmail.com>
2016-07-19 22:47:20 +0700

    add Retrofit1Activity and code for retrieve data using retrofit 1.9

---
+++ app/src/main/java/com/triwayuprasetyo/mapretrofitglidesdp/Retrofit1Activity.java
@@ -0,0 +1,89 @@
+package com.triwayuprasetyo.mapretrofitglidesdp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.triwayuprasetyo.mapretrofitglidesdp.retrofit.AnggotaInterface;
+import com.triwayuprasetyo.mapretrofitglidesdp.retrofit.AnggotaWrapper;
+
+import retrofit.Callback;
+import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+public class Retrofit1Activity extends AppCompatActivity implements View.OnClickListener {
+
+    private ListView listViewRetrofit;
+    private String[] daftarNama;
+    private Button buttonAddRetrofit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_retrofit1);
+
+        setTitle("Retrofit 1.9");
+
+        listViewRetrofit = (ListView) findViewById(R.id.listview_retrofit);
+        buttonAddRetrofit = (Button) findViewById(R.id.button_add_retrofit);
+        buttonAddRetrofit.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        retrieveData();
+        Log.i("SDP Retrofit", "On Resume Method");
+    }
+
+    private void retrieveData() {
+        //Retrofit 1.9
+        RestAdapter restAdapter = new RestAdapter.Builder()
+                .setEndpoint("http://triwahyuprasetyo.xyz")
+                .build();
+        AnggotaInterface anggotaService = restAdapter.create(AnggotaInterface.class);
+        anggotaService.getDataAnggota(new Callback<AnggotaWrapper>() {
+            @Override
+            public void success(AnggotaWrapper anggotaWrapper, Response response) {
+                Toast.makeText(getApplicationContext(), "Retrieve Success", Toast.LENGTH_SHORT).show();
+                String[] daftarNama = new String[anggotaWrapper.getAnggota().size()];
+                int i = 0;
+                for (AnggotaWrapper.Anggota anggota : anggotaWrapper.getAnggota()) {
+                    Log.d("SDP", "Anggota :: " + anggota.getId());
+                    Log.d("SDP", "Anggota :: " + anggota.getNama());
+                    Log.d("SDP", "Anggota :: " + anggota.getAlamat());
+                    Log.d("SDP", "Anggota :: " + anggota.getUsername());
+                    Log.d("SDP", "Anggota :: " + anggota.getPassword());
+                    Log.d("SDP", "=======================================");
+                    daftarNama[i] = anggota.getNama();
+                    i++;
+                }
+                listViewRetrofit.setAdapter(new ArrayAdapter(Retrofit1Activity.this, android.R.layout.simple_list_item_1, daftarNama));
+                listViewRetrofit.invalidate();
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                Toast.makeText(getApplicationContext(), "Retrieve Error", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                Log.d("SDP", "Error :: " + error.getMessage());
+            }
+
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == buttonAddRetrofit.getId()) {
+            Intent i = new Intent(getApplicationContext(), AddAnggotaActivity.class);
+            startActivity(i);
+        }
+    }
+}
---
+++ app/src/main/java/com/triwayuprasetyo/mapretrofitglidesdp/retrofit/AnggotaInterface.java
@@ -0,0 +1,33 @@
+package com.triwayuprasetyo.mapretrofitglidesdp.retrofit;
+
+import retrofit.Callback;
+import retrofit.client.Response;
+import retrofit.http.Body;
+import retrofit.http.Field;
+import retrofit.http.FormUrlEncoded;
+import retrofit.http.GET;
+import retrofit.http.POST;
+
+/**
+ * Created by sdp03 on 7/18/16.
+ */
+
+public interface AnggotaInterface {
+    //Retrofit 1.9
+    @GET("/daftaranggota.php")
+    void getDataAnggota(Callback<AnggotaWrapper> callback);
+
+    @POST("/addanggota.php")
+    void tambahAnggota(@Body AnggotaWrapper.Anggota body, Callback<AnggotaWrapper.Anggota> callBack);
+
+    @FormUrlEncoded
+    @POST("/addanggota.php")
+    void tambahPostAnggota(@Field("nama") String nama,
+                           @Field("username") String username,
+                           @Field("password") String password,
+                           @Field("alamat") String alamat,
+                           @Field("latitude") String latitude,
+                           @Field("longitude") String longitude,
+                           @Field("foto") String foto,
+                           Callback<Response> callback);
+}
