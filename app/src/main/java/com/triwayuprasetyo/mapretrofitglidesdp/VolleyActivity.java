7c7cb195bffe22bad55ff8881d508151be05098e
why <triwahyuprasetyo@gmail.com>
2016-07-26 07:18:22 +0700

    Add VolleyActivity

---
+++ app/src/main/java/com/triwayuprasetyo/mapretrofitglidesdp/VolleyActivity.java
@@ -0,0 +1,13 @@
+package com.triwayuprasetyo.mapretrofitglidesdp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VolleyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_volley);
+    }
+}
