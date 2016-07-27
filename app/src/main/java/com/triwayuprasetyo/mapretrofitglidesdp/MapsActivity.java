c68ccac085176d685130b7edf503710629e72124
why <triwahyuprasetyo@gmail.com>
2016-07-27 09:57:24 +0700

    Change location to uns

--- app/src/main/java/com/triwayuprasetyo/mapretrofitglidesdp/MapsActivity.java
+++ app/src/main/java/com/triwayuprasetyo/mapretrofitglidesdp/MapsActivity.java
@@ -39,11 +39,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        // Enabling MyLocation Layer of Google Map
+        //mMap.setMyLocationEnabled(true);
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng uns = new LatLng(-7.561577, 110.856582);
+        mMap.addMarker(new MarkerOptions().position(uns).title("Univ 11 Maret"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(uns));
 //        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
 //            @Override
 //            public boolean onMarkerClick(Marker marker) {
