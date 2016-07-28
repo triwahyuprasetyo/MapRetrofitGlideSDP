e083cc8657270b497470247fe44911cbb54c3048
why <triwahyuprasetyo@gmail.com>
2016-07-28 12:34:23 +0700

    Update VolleyActivity, comment service, notification, broadcast part

--- app/src/main/java/com/triwayuprasetyo/mapretrofitglidesdp/VolleyActivity.java
+++ app/src/main/java/com/triwayuprasetyo/mapretrofitglidesdp/VolleyActivity.java
@@ -27,20 +27,20 @@ import org.json.JSONObject;
 
 public class VolleyActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private static final int ID_NOTIFICATION = 1;
-    static VolleyActivity va;
-    private static BroadcastReceiver getProcessStatus = new BroadcastReceiver() {
-
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            Bundle extras = intent.getExtras();
-            if (extras != null) {
-                final String message = extras.getString(GlobalConst.ID_RESULT_FROM_OTHER_PROCESS);
-                Log.i("SDP NOTIFICATION", "");
-                va.generateNotification(message);
-            }
-        }
-    };
+//    private static final int ID_NOTIFICATION = 1;
+//    static VolleyActivity va;
+//    private static BroadcastReceiver getProcessStatus = new BroadcastReceiver() {
+//
+//        @Override
+//        public void onReceive(Context context, Intent intent) {
+//            Bundle extras = intent.getExtras();
+//            if (extras != null) {
+//                final String message = extras.getString(GlobalConst.ID_RESULT_FROM_OTHER_PROCESS);
+//                Log.i("SDP NOTIFICATION", "");
+//                va.generateNotification(message);
+//            }
+//        }
+//    };
     private Button buttonGetAnggota;
     private String[] daftarId, daftarNama, daftarAlamat, daftarUsername, daftarPassword, daftarLatitude, daftarLongitude, daftarFoto;
 
@@ -111,64 +111,64 @@ public class VolleyActivity extends AppCompatActivity implements View.OnClickLis
         }
     }
 
-    public void generateNotification(String message) {
-        Context context = getApplicationContext();
-
-        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.cancel(ID_NOTIFICATION);//Untuk menutup notifikasi yang ada (dengan id yang disebutkan), ditutup dulu.
-
-        long when = System.currentTimeMillis();
-
-        String contentTitle = "Notification";
-        boolean is_notif_sound = true;
-
-        Bitmap icon_big = BitmapFactory.decodeResource(context.getResources(), R.mipmap.ic_launcher);
-
-//        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB)
-//        {
+//    public void generateNotification(String message) {
+//        Context context = getApplicationContext();
 //
-//            Notification notification = new Notification(R.mipmap.ic_launcher, message, when); // deprecated in API level 11
-//            Intent toLaunch = new Intent(context,LauncherActivity.class);
-//            toLaunch.putExtra("NOTIFICATION",true);
+//        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+//        notificationManager.cancel(ID_NOTIFICATION);//Untuk menutup notifikasi yang ada (dengan id yang disebutkan), ditutup dulu.
 //
-//            PendingIntent contentIntent = PendingIntent.getActivity(context, 0, toLaunch, PendingIntent.FLAG_UPDATE_CURRENT);
-//            notification.setLatestEventInfo(context, contentTitle, message, contentIntent); // deprecated in API level 11
-//            //Setting Notification Flags
-//            notification.flags |= Notification.FLAG_AUTO_CANCEL;
-//            if (is_notif_sound) {
-//                notification.sound = Uri.parse("android.resource://com.yandi.mp3player/" + R.raw.pop);
-//            }
-//            //vibrate permission harus diregister dimanifest
-//            //notification.vibrate = new long[]{200, 200, 200, 200, 200};
-//            notificationManager.notify(1, notification);
-//        }
+//        long when = System.currentTimeMillis();
 //
-//        else{
-
-        Intent notificationIntent = new Intent(context, TambahAnggotaActivity.class);
-        notificationIntent.putExtra("NOTIFICATION", true);
-
-        PendingIntent intent = PendingIntent.getActivity(context, 1, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
-        inboxStyle.addLine(message);
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context);
-        mBuilder.setContentTitle(contentTitle)
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setLargeIcon(icon_big)
-                .setSubText("Sub Text here")
-                .setContentInfo("")
-                .setWhen(when)
-                .setAutoCancel(true)
-                .setContentIntent(intent)
-                .setStyle(inboxStyle)
-                //vibrate permission harus diregister dimanifest
-                //.setVibrate(new long[]{200,200,200,200,200})
-                .setTicker(message);
-
-        // if (is_notif_sound)
-        //mBuilder.setSound(Uri.parse("android.resource://com.yandi.mp3player/" + R.raw.pop));
-
-        notificationManager.notify(1, mBuilder.build());
-//        }
-    }
+//        String contentTitle = "Notification";
+//        boolean is_notif_sound = true;
+//
+//        Bitmap icon_big = BitmapFactory.decodeResource(context.getResources(), R.mipmap.ic_launcher);
+//
+////        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB)
+////        {
+////
+////            Notification notification = new Notification(R.mipmap.ic_launcher, message, when); // deprecated in API level 11
+////            Intent toLaunch = new Intent(context,LauncherActivity.class);
+////            toLaunch.putExtra("NOTIFICATION",true);
+////
+////            PendingIntent contentIntent = PendingIntent.getActivity(context, 0, toLaunch, PendingIntent.FLAG_UPDATE_CURRENT);
+////            notification.setLatestEventInfo(context, contentTitle, message, contentIntent); // deprecated in API level 11
+////            //Setting Notification Flags
+////            notification.flags |= Notification.FLAG_AUTO_CANCEL;
+////            if (is_notif_sound) {
+////                notification.sound = Uri.parse("android.resource://com.yandi.mp3player/" + R.raw.pop);
+////            }
+////            //vibrate permission harus diregister dimanifest
+////            //notification.vibrate = new long[]{200, 200, 200, 200, 200};
+////            notificationManager.notify(1, notification);
+////        }
+////
+////        else{
+//
+//        Intent notificationIntent = new Intent(context, TambahAnggotaActivity.class);
+//        notificationIntent.putExtra("NOTIFICATION", true);
+//
+//        PendingIntent intent = PendingIntent.getActivity(context, 1, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+//        NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
+//        inboxStyle.addLine(message);
+//        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context);
+//        mBuilder.s`etContentTitle(contentTitle)
+//                .setSmallIcon(R.mipmap.ic_launcher)
+//                .setLargeIcon(icon_big)
+//                .setSubText("Sub Text here")
+//                .setContentInfo("")
+//                .setWhen(when)
+//                .setAutoCancel(true)
+//                .setContentIntent(intent)
+//                .setStyle(inboxStyle)
+//                //vibrate permission harus diregister dimanifest
+//                //.setVibrate(new long[]{200,200,200,200,200})
+//                .setTicker(message);
+//
+//        // if (is_notif_sound)
+//        //mBuilder.setSound(Uri.parse("android.resource://com.yandi.mp3player/" + R.raw.pop));
+//
+//        notificationManager.notify(1, mBuilder.build());
+////        }
+//    }
 }
